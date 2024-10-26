aa637d64b7be5a15a90af748f3bce6dd3478d7d0
Hans2374 <minhkhoi23704@gmail.com>
2024-10-26 18:56:51 +0700

    responsive 2.0

--- src/components/Login.js
+++ src/components/Login.js
@@ -144,9 +144,9 @@ const Login = () => {
     };
 
     const handleForgotPassword = () => {
+        setIsForgotPassword(true);
         setAnimate(true);
         setTimeout(() => {
-            setIsForgotPassword(true);
             setUsername('');
             setPassword('');
             setConfirmPassword('');
@@ -221,13 +221,13 @@ const Login = () => {
                 >
                     <Typography className={styles.companyName} variant="h6" style={{ color: colors.color3, fontSize: '32px', fontWeight: 700, marginBottom: '40px' }}>LOGO/TÊN</Typography>
                     <Box className={styles.slideText}>
-                        <Typography variant="h6" className={`${styles.welcome} ${isRegister ? (animate ? styles.slideUp : styles.slideDown) : ''}`} style={{ color: colors.color1, fontSize: '30px', fontWeight: 600, marginBottom: '10px' }}>
+                        <Typography variant="h6" className={`${styles.welcome} ${!isForgotPassword && !isResetPassword ? (animate ? styles.slideUp : styles.slideDown) : ''}`} style={{ color: colors.color1, fontSize: '30px', fontWeight: 600, marginBottom: '10px' }}>
                             {isRegister ? 'Chào mừng gia nhập!' : 'Chào mừng trở lại!'}
                         </Typography>
                     </Box>
                     <Typography className={styles.leftDescription} variant="h6" style={{ color: colors.color1, fontSize: '20px', fontWeight: 400, marginBottom: '50px' }}>Cùng chung tay bảo vệ môi trường với những sản phẩm tái chế và dịch vụ của chúng tôi. Hãy bắt đầu ngay để biến những điều cũ thành mới!</Typography>
                     <Box className={styles.slideText}>
-                        <Typography variant="h6" className={`${isRegister ? (animate ? styles.slideUp : styles.slideDown) : ''}`} style={{ color: colors.color1, fontSize: '15px', fontWeight: 400 }}>
+                        <Typography variant="h6" className={`${!isForgotPassword && !isResetPassword ? (animate ? styles.slideUp : styles.slideDown) : ''}`} style={{ color: colors.color1, fontSize: '15px', fontWeight: 400 }}>
                             {isRegister ? 'Đã có tài khoản?' : 'Chưa có tài khoản?'}
                         </Typography>
                     </Box>
