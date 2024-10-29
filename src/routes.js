c0c69cd25ce4b50b56cc6330bf21c445b3098ee1
ThinhPST <142957864+ThinhPST@users.noreply.github.com>
2024-10-29 11:24:32 +0700

    product

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 // src/routes.js
-import LoginPage from './pages/login';
-import NewsPage from './pages/news';
-import NewsDetailPage from './pages/newsDetail';
-import HomePage from './pages/homePage';
+import LoginPage from "./pages/login";
+import NewsPage from "./pages/news";
+import NewsDetailPage from "./pages/newsDetail";
+import HomePage from "./pages/homePage";
+import ProductPage from "./pages/Product";
 
 const routes = [
-  { path: '/', element: <HomePage /> }, 
-  { path: '/login', element: <LoginPage /> },
-  { path: '/home', element: <HomePage /> },
-  { path: '/news', element: <NewsPage /> },
-  { path: '/news-detail', element: <NewsDetailPage /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/home", element: <HomePage /> },
+  { path: "/news", element: <NewsPage /> },
+  { path: "/news-detail", element: <NewsDetailPage /> },
+  { path: "/product", element: <ProductPage /> },
 ];
 
 export default routes;
