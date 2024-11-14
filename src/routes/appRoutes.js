f62c9053093c2694ecdcf0ba190c61d6e36306a7
Hans2374 <minhkhoi23704@gmail.com>
2024-11-14 15:14:38 +0700

    route

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -10,7 +10,13 @@ import Regulation from "../pages/regulation";
 import Product from "../components/Product";
 import ProductDetail from "../pages/ProductDetail";
 import Profile from "../components/Profilepage";
-import ProductPage from "../components/Product";
+import Sell from "../pages/sell";
+import Cart from "../pages/cart";
+import ClassifyDetail from "../pages/classifyDetail";
+import AdminOrderPage from "../pages/Admin/AdminOrderPage";
+import AdminProductPage from "../pages/Admin/AdminProductPage";
+import AdminStatisticPage from "../pages/Admin/AdminStatisticPage";
+import AdminUserPage from "../pages/Admin/AdminUserPage";
 
 export default function AppRoute() {
   return (
@@ -25,7 +31,13 @@ export default function AppRoute() {
         <Route path={routes.product} element={<Product />} />
         <Route path="/product/:slug" element={<ProductDetail />} />
         <Route path={routes.profile} element={<Profile />} />
-
+        <Route path={routes.sell} element={<Sell />} />
+        <Route path={routes.cart} element={<Cart />} />
+        <Route path={routes.classifyDetail} element={<ClassifyDetail />} />
+        <Route path={routes.adminproduct} element={<AdminProductPage />} />
+        <Route path={routes.adminorder} element={<AdminOrderPage />} />
+        <Route path={routes.adminstatistic} element={<AdminStatisticPage />} />
+        <Route path={routes.adminuser} element={<AdminUserPage />} />
       </Routes>
     </>
   );
